2aa8880be2644a9ca0a88987e0fe3cbb6363474b
Lucas Diniz <lucazdinis@gmail.com>
2018-07-12 01:22:21 -0300

    Adds assignment rules

--- eclipseWorkspace/GoTranslator/test/input1.go
+++ eclipseWorkspace/GoTranslator/test/input1.go
@@ -3,19 +3,36 @@ package main
 import "fmt"
 
 func plus(j int, b int) int {
-
-    return a + b
+    x = 3
+    return x
 }
 
 func plusPlus(a, b, c int) int {
-    return a + b + c
+    1 << 3
+    x = 1
+    return a + b - c
 }
 
 func main() {
+    yo = calc
+    (1+3) >> 3
+    yo = calc()
+    // x
+    // x.o
+    //classe.metodo(classe2.metodo2(a))
+    // int(a)
+    // x.b()
+    // lkk(1)
+    // lkk(1,2,3)
+
+    carlos += 1<<3 + (4 * 1)
+    xd,lol = po,x.calc()
+    // calc(1,2,3)
 
     // res := plus(1, 2)
-    fmt.Println("1+2 =", res)
+    // fmt.Println("1+2 =", res)
 
     // res = plusPlus(1, 2, 3)
-    fmt.Println("1+2+3 =", res)
+    // fmt.Println("1+2+3 =", res)
+    // return k + 10
 }
